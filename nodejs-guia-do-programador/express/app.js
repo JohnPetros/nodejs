d31b1fb1a3c4706f123b3350efc0f790589d41cb
João Pedro <joaopedro.nc@outlook.com>
2022-10-07 14:50:25 -0300

    Prarâmetros

--- nodejs-guia-do-programador/express/app.js
+++ nodejs-guia-do-programador/express/app.js
@@ -12,6 +12,21 @@ app.get("/blog", function (req, res) {
   res.send("Meu blog");
 });
 
+app.get("/ola/:nome/:cargo/:idade", function (req, res) {
+  //   res.send(req.params);
+  res.send(
+    "<h1>Olá " +
+      req.params.nome +
+      "</h1>" +
+      "<h2>Seu cargo é " +
+      req.params.cargo +
+      "</h2>" +
+      "<h2>Sua idade é " +
+      req.params.idade +
+      "</h2>"
+  );
+});
+
 app.listen(8081, function (req, res) {
   console.log("Servidor rodando na url http://localhost:8081");
 });
