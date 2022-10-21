029910828c1e7653e815edb67f3c7268245b0743
João Pedro <joaopedro.nc@outlook.com>
2022-10-21 18:00:37 -0300

    Bootstrap com Express

--- nodejs-guia-do-programador/blog-app/app.js
+++ nodejs-guia-do-programador/blog-app/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
 const app = express();
-const admin = require("./routes/admin")
+const admin = require("./routes/admin");
+const path = require("path");
 // const mongoose = require("mongoose");
 
 // Config
@@ -26,8 +27,12 @@ app.set("view engine", "handlebars");
 
 // Mongoose
 
+
+// Public
+app.use(express.static(path.join(__dirname, "public")))
+
 // Routes
-app.use("/admin", admin)
+app.use("/admin", admin);
 
 // Others
 const PORT = 8081;
--- nodejs-guia-do-programador/blog-app/routes/admin.js
+++ nodejs-guia-do-programador/blog-app/routes/admin.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Página principal")
-})
+  res.render("admin/index");
+});
 
 router.get("/posts", (req, res) => {
-    res.send("Página de posts")
-})
+  res.send("Página de posts");
+});
 
 router.get("/categorias", (req, res) => {
-    res.send("Página de categorias")
-})
+  res.send("Página de categorias");
+});
 
-module.exports = router
+module.exports = router;
