a48492e48573a317d6f0f499c63949d225350e3b
João Pedro <joaopedro.nc@outlook.com>
2022-10-27 11:42:00 -0300

    Cadastrando categorias no banco de dados

--- nodejs-guia-do-programador/blog-app/app.js
+++ nodejs-guia-do-programador/blog-app/app.js
@@ -43,6 +43,10 @@ mongoose
 // Public
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use((req, res, next) => {
+  console.log("middleware");
+})
+
 // Routes
 app.use("/admin", admin);
 
--- nodejs-guia-do-programador/blog-app/models/Category.js
+++ nodejs-guia-do-programador/blog-app/models/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-new Category = new Schema({
+const Category = new Schema({
     name: {
         type: String,
         required: true,
